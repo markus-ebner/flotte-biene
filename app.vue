f934ced18704d21a5fbb201b493979acc758b5f3
markusebner <markus.ebner@redlink.at>
2023-12-22 11:38:42 +0100

    better version

--- app.vue
+++ app.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="bg-[#FDCE29] min-h-screen min-w-screen flex items-center justify-center flex-col gap-10">
+  <div class="p-4 bg-[#FDCE29] text-[#7A5903] min-h-screen min-w-screen flex items-center justify-center flex-col gap-4 sm:gap-10">
     <div class="flex">
       <img class="rotate-90 w-20" src="public/biene.svg" alt="Flotte Biene Logo">
       <div >
-        <h1 class="text-4xl font-extrabold">Flotte Biene</h1>
-        <p class="block">Webshop für Natur- & Bienenprodukte</p>
+        <h1 class="text-4xl sm:text-6xl font-extrabold">Flotte Biene</h1>
+        <p class="block sm:text-xl">Webshop für Natur-, Textil- & Bienenprodukte</p>
       </div>
     </div>
-    <div class="flex gap-4">
-      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Bienenhonig">
-      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Scrunchies">
-      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Cremen">
+    <div class="flex gap-4 flex-wrap justify-center">
+      <img class="w-full sm:w-60 sm:h-60 object-cover rounded-2xl border border-[#7A5903] border-8" src="public/honig.jpeg" alt="Bienenhonig">
+      <img class="w-full sm:w-60 sm:h-60 object-cover rounded-2xl border border-[#7A5903] border-8" src="public/ernte.jpg" alt="Scrunchies">
+      <img class="w-full sm:w-60 sm:h-60 object-cover rounded-2xl border border-[#7A5903] border-8" src="public/bienenbeuten.jpg" alt="Cremen">
     </div>
   </div>
 </template>
 <script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'Flotte Biene Shop', content:  'Webshop für Natur-, Textil- & Bienenprodukte'}
+  ],
+})
 </script>

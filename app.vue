113887316f19a0ef3a3723141936bd31306f75f4
markusebner <markus.ebner@redlink.at>
2023-12-22 08:17:08 +0100

    minimal version

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-[#FDCE29] min-h-screen min-w-screen flex items-center justify-center flex-col gap-10">
+    <div class="flex">
+      <img class="rotate-90 w-20" src="public/biene.svg" alt="Flotte Biene Logo">
+      <div >
+        <h1 class="text-4xl font-extrabold">Flotte Biene</h1>
+        <p class="block">Webshop für Natur- & Bienenprodukte</p>
+      </div>
+    </div>
+    <div class="flex gap-4">
+      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Bienenhonig">
+      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Scrunchies">
+      <img class="w-40 h-40 object-cover rounded-2xl border border-black border-8" src="public/honey.jpg" alt="Cremen">
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
